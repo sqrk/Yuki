1feb9b8ad9b35c5cb9b8253f756c1697e7cb277f
Karima Kadaoui <kariima.kad@gmail.com>
2020-02-26 14:59:21 +0100

    First commit

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Yuki</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      My App
+    </ion-content>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
