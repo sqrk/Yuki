083ecdbe6958a075346334ac3ab80df21cd385b4
Karima Kadaoui <kariima.kad@gmail.com>
2020-04-16 21:35:22 +0100

    Profile layout

--- client/src/views/ProfilePage.vue
+++ client/src/views/ProfilePage.vue
@@ -1,19 +1,40 @@
 <template lang="pug">
-  .profile-page
-    | Username: {{ $store.state.user.username }}
-    | Email: {{ $store.state.user.email }}
-    button.btn-danger(@click="logout") Logout
-    .error {{ this.error }}
+  .profile-page.page
+    .container
+      h1.center Profile
+      .info.card
+        .score.bg.btn-primary {{ user.score }}
+        i.edit.far.fa-edit
+        .row
+          .col-6
+            label Username:
+          .col-6
+            label {{ user.username }}
+        .row
+          .col-6
+            label Email:
+          .col-6
+            label {{ user.email }}
+        .row
+          .col-6
+            label Password:
+          .col-6
+            label *******
+      button.btn-danger.logout(@click="logout") Logout
+      .error {{ this.error }}
 </template>
 
 <script>
 import AuthenticationService from "../services/AuthenticationService";
-
+// TODO Remove pendingTestimonial
+// TODO Score
+// TODO Add level
 export default {
   name: "ProfilePage",
   data() {
     return {
-      error: null
+      error: null,
+      user: this.$store.state.user
     };
   },
   methods: {
@@ -31,4 +52,35 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+@import "../assets/css/variables";
+
+.info {
+  padding: 25px 20px;
+  position: relative;
+  label {
+    display: block;
+  }
+  .score {
+    position: absolute;
+    right: -10px;
+    top: -23px;
+  }
+  .edit {
+    position: absolute;
+    height: 45px;
+    width: 63px;
+    border: 3px solid $primary-color;
+    background: white;
+    padding: 10px 20px;
+    border-radius: 12px;
+    top: -23px;
+    left: -10px;
+    color: $primary-color;
+  }
+}
+.logout {
+  display: block;
+  margin: 15px auto;
+}
+</style>
